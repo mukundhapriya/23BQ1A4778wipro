9076477f11e3da2b202ca442762d405dfdfbcfff
Mukundhapriya <sankumukundhapriya@gmail.com>
2026-07-17 09:00:24 +0530

    Remove duplicate project folder

--- 23BQ1A4778/src/fundamentals/Main.java
+++
@@ -1,7 +0,0 @@
-package fundamentals;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println(args[0] + " Technologies " + args[1]);
-	}
-}
--- 23BQ1A4778/src/fundamentals/WelcomeMessage.java
+++
@@ -1,9 +0,0 @@
-package fundamentals;
-
-public class WelcomeMessage {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome " + args[0]);
-	}
-
-}
