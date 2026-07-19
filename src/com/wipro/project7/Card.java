1cf18fdafbfc35071ee67ea8e8b9e02f4b728907
Mukundhapriya <sankumukundhapriya@gmail.com>
2026-07-19 20:22:32 +0530

    project7

---
+++ src/com/wipro/project7/Card.java
@@ -0,0 +1,25 @@
+package com.wipro.project7;
+
+public class Card {
+
+    private char symbol;
+    private int number;
+
+    public Card(char symbol, int number) {
+        this.symbol = symbol;
+        this.number = number;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return symbol + " " + number;
+    }
+}
