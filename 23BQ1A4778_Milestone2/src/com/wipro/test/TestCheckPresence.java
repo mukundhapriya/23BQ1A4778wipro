2273ce0fdab899ff3d462a28308b9c9c32762009
Mukundhapriya <sankumukundhapriya@gmail.com>
2026-07-28 19:38:58 +0530

    Milestone 2 - JUnit Assignments Completed

---
+++ 23BQ1A4778_Milestone2/src/com/wipro/test/TestCheckPresence.java
@@ -0,0 +1,21 @@
+package com.wipro.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.wipro.task.DailyTasks;
+
+public class TestCheckPresence {
+
+    @Test
+    public void testCheckPresence() {
+
+        DailyTasks obj = new DailyTasks();
+
+        assertTrue(obj.checkPresence("Wipro Technologies", "Tech"));
+
+        assertFalse(obj.checkPresence("Wipro Technologies", "Java"));
+    }
+}
