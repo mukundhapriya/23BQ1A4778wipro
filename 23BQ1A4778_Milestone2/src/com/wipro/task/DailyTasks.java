c887fe96216e4a9c4a4528f39f54c9c07eb21abd
Mukundhapriya <sankumukundhapriya@gmail.com>
2026-07-28 18:26:01 +0530

    Milestone 2 - Initial Setup and JUnit Assignment 1

---
+++ 23BQ1A4778_Milestone2/src/com/wipro/task/DailyTasks.java
@@ -0,0 +1,22 @@
+package com.wipro.task;
+
+import java.util.Arrays;
+
+public class DailyTasks {
+
+    // Assignment 1
+    public String doStringConcat(String s1, String s2) {
+        return s1 + s2;
+    }
+
+    // Assignment 2
+    public int[] sortValues(int[] arr) {
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    // Assignment 3
+    public boolean checkPresence(String str, String a) {
+        return str.contains(a);
+    }
+}
