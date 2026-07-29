c9e4959d31da44b4230efb2d959932d17fd36f7d
Mukundhapriya <sankumukundhapriya@gmail.com>
2026-07-29 11:39:26 +0530

    update assignemnt

---
+++ 23BQ1A4778_Milestone2/src/com/wipro/threads/ThreadDemo.java
@@ -0,0 +1,16 @@
+package com.wipro.threads;
+
+public class ThreadDemo {
+
+    public static void main(String[] args) {
+
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+
+        t1.setName("Scooby");
+        t2.setName("Shaggy");
+
+        t1.start();
+        t2.start();
+    }
+}
